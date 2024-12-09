59836399f333b1e06dd00d01a8c9018ef338a173
Allie Siclari <alliesiclari@yahoo.com>
2024-12-09 13:21:15 -0500

    map search bar

--- js/index.js
+++ js/index.js
@@ -13,12 +13,26 @@ const marker = L.marker([44.4759, -73.2121]).addTo(map);
 // Add a popup to the marker
 marker.bindPopup("<b>Downtown Burlington</b><br>Welcome to Burlington, VT!").openPopup();
 
-// Add a circle around downtown
-L.circle([44.4759, -73.2121], {
-  color: 'blue',
-  fillColor: '#30f',
-  fillOpacity: 0.4,
-  radius: 1000, // Radius in meters
+// Add a search bar using Leaflet Control Geocoder
+L.Control.geocoder({
+  defaultMarkGeocode: false,
+  collapsed: false, // Keeps the search bar expanded
+}).on('markgeocode', function (e) {
+  const bbox = e.geocode.bbox;
+  const poly = L.polygon([
+    [bbox.getSouthEast().lat, bbox.getSouthEast().lng],
+    [bbox.getNorthEast().lat, bbox.getNorthEast().lng],
+    [bbox.getNorthWest().lat, bbox.getNorthWest().lng],
+    [bbox.getSouthWest().lat, bbox.getSouthWest().lng]
+  ]).addTo(map);
+
+  // Center the map on the selected location
+  map.fitBounds(poly.getBounds());
+
+  // Add a marker at the searched location
+  L.marker(e.geocode.center).addTo(map)
+    .bindPopup(`<b>${e.geocode.name}</b>`)
+    .openPopup();
 }).addTo(map);
 
 
